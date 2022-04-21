ca884a41c9abe79c73aa03e6fa134e91aa49af70
cpalomino1990 <carlospalominoes@gmail.com>
2022-04-21 00:04:51 -0500

    first commit

---
+++ ejercicio1.js
@@ -0,0 +1,15 @@
+var casas = [1, 1, 1, 0, 1, 1, 1, 1];
+var dias = 2;
+
+function calcularEstadoCeldas(casas, dias) {
+  var salida = [];
+  if (dias === 0) return casas;
+
+  for (let i = 0; i < casas.length; i++) {
+    salida[i] = (casas[i - 1] || 0) === (casas[i + 1] || 0) ? 0 : 1;
+  }
+
+  return calcularEstadoCeldas(salida, dias - 1);
+}
+
+console.log(calcularEstadoCeldas(casas, dias));
